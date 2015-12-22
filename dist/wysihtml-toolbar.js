618101ac4ca2c376aa57af12cc35eb633a0cb415
Sebastian Ekström <seb.ekstrom@gmail.com>
2015-12-22 14:04:49 +0100

    Remove fix

--- dist/wysihtml-toolbar.js
+++ dist/wysihtml-toolbar.js
@@ -469,11 +469,7 @@ var wysihtml5 = {
       if (anode && anode.parentNode && fnode && fnode.parentNode) {
         r.setStart(anode, aoffset);
         r.setEnd(fnode, foffset);
-        if(s.getRangeAt(0)) {
-          if (s.getRangeAt(0).getClientRects.length > 0) { // Avoid IE error 800a025e
-              s.removeAllRanges();
-          }
-        }
+        s.removeAllRanges();
         s.addRange(r);
       }
     };
@@ -3472,12 +3468,8 @@ wysihtml5.polyfills(window, document);
 
                     // Clean up
                     dom.removeNode(testEl);
-                    if(sel.getRangeAt(0)) {
-                      if (sel.getRangeAt(0).getClientRects.length > 0) { // Avoid IE error 800a025e
-                        sel.removeAllRanges();
-                      }                      
-                    }
-
+                    sel.removeAllRanges();
+                    
                     for (i = 0; i < originalSelectionRangeCount; ++i) {
                         if (i == 0 && originalSelectionBackward) {
                             if (addRangeBackwardToNative) {
